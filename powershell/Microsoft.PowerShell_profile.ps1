29423573817728bedf9529a5e7af69593d441939
Miguel Juan <miguelkarlojuan@gmail.com>
2022-04-14 12:16:10 +1200

    change windows terminal theme

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -4,7 +4,7 @@
 
 # Prettify the Commandline
 Import-Module -Name Terminal-Icons
-oh-my-posh.exe init pwsh --config $env:POSH_THEMES_PATH\rudolfs-dark.omp.json | Invoke-Expression
+oh-my-posh.exe init pwsh --config $env:POSH_THEMES_PATH\space.omp.json | Invoke-Expression
 
 # Configure PowerShell history
 Import-Module PSReadLine -Version 2.2.2
