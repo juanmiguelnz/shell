3977e0de3e73865660ff7ed35a56539c48985781
Miguel Juan <miguelkarlojuan@gmail.com>
2022-04-14 12:15:12 +1200

    update powershell

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -3,8 +3,8 @@
 # Install-Module -Name PSReadLine -AllowClobber -Force
 
 # Prettify the Commandline
-oh-my-posh.exe init pwsh --config $env:POSH_THEMES_PATH\rudolfs-dark.omp.json | Invoke-Expression
 Import-Module -Name Terminal-Icons
+oh-my-posh.exe init pwsh --config $env:POSH_THEMES_PATH\rudolfs-dark.omp.json | Invoke-Expression
 
 # Configure PowerShell history
 Import-Module PSReadLine -Version 2.2.2
